48c8a16c4c3b29a2d0ec9c7fd48ff3e078685eaa
k-pine-su <kaker@LAPTOP-E65535.localdomain>
2025-07-11 17:14:12 +0900

    07/11 17:14

---
+++ src/10steptoymodel.jl
@@ -0,0 +1,25 @@
+module RandomWork
+
+# ランダムウォークのシミュレーションを行う関数
+# directionで進む方向x,y,zをランダムに選び，進む量もランダムで選ぶ
+function randomwalk()
+    x, y, z = 0, 0, 0
+    steps = 10
+    for i in 1:steps
+        direction = rand(["x_direction", "y_direction", "z_direction"])
+        if direction == "x_direction"
+            x += rand([-1, 1])
+        elseif direction == "y_direction"
+            y += rand([-1, 1])
+        else # direction == "z_direction"
+            z += rand([-1, 1])
+        end
+        println("Step $i: ($x, $y, $z)")
+    end    
+    return (x, y, z)
+end
+
+# 関数の呼び出し
+randomwalk()
+
+end
